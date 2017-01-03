753f82f827ddd50a0b678ca7cff62a402b248421
asd <asd@localhost.localdomain>
2017-01-03 15:10:36 +0530

    adding some site updates

--- js/index.js
+++
@@ -1,9 +0,0 @@
-function thanks(){
-  var nameVal = document.getElementById('name').checkValidity();
-  var emailVal = document.getElementById('email').checkValidity();
-  var textareaVal = document.getElementById('message').checkValidity();
-  if(nameVal && emailVal && textareaVal){
-    document.getElementById('sendButton').style.width = "97px";
-    document.getElementById('sendButton').setAttribute('value','thanks!');
-  }
-}
